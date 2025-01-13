b2e4fb1d867d320ec185954a9304d59d4af06986
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2025-01-13 12:33:54 -0500

    first commit

---
+++ src/main/java/com/sintad/management/PruebaSintadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sintad.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSintadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSintadBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sintad/management/PruebaSintadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sintad.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSintadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
